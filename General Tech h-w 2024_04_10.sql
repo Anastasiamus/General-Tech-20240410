b95a5d4368aa0eb12f135284115d451f39a6da34
Anastasia Sobyanina <145497509+Anastasiamus@users.noreply.github.com>
2024-04-17 10:43:02 +0200

    Add files via upload

---
+++ General Tech h-w 2024_04_10.sql
@@ -0,0 +1,44 @@
+use airport1;
+
+-- Посчитать количество рейсов по каждой модели самолета. Вывести modelname и flightcount.
+
+select  model_name as modelname, 
+		count(*) as flightcount
+from airliners
+group by model_name;
+
+-- Вычислить среднюю стоимость билета по классам обслуживания. Вывести serviceclass и averageprice.
+
+select service_class as serviceclass, 
+		avg(price) as averageprice
+from tickets
+group by service_class;
+        
+
+-- Определить средний возраст клиентов, сгруппированных по классу обслуживания. Вывести serviceclass и averageage.
+select avg(t1.age) as averageage, t2.service_class
+from clients t1
+join tickets t2 on t1.id=t2.client_id
+group by t2.service_class;
+
+-- Получить общее количество билетов, купленных каждым клиентом. Вывести name и ticket_count.
+
+select count(*) as ticket_count, t2.name
+from tickets t1
+join clients t2 on t1.client_id=t2.id
+group by t2.name;
+		
+
+-- Определить количество рейсов, отправленных из каждого аэропорта. Вывести departure и departure_count.
+
+select count(*) as departure_count, departure
+from trips 
+group by departure;
+
+-- Вычислить общий доход от продажи билетов для каждой страны производителя самолетов. Вывести country и total_sum.
+
+select t1.country, sum(t3.price) as total_sum
+from airliners t1
+join trips t2 on t1.id=t2.airliner_id
+join tickets t3 on t2.id= t3.trip_id
+group by t1.country;
